eaeba1db437a44550efe1216d0bc3bc0760c3a21
ZeeHatcher <zee.hatcher010@gmail.com>
2023-07-08 17:36:28 +0800

    feat: upload premades

---
+++ src/ui/main_menu/components/credit_group/credit_group.gd
@@ -0,0 +1,32 @@
+@tool
+extends VBoxContainer
+
+
+@export var title := "Title" : set = _set_title
+@export var names: Array[String] = [] : set = _set_names
+
+@onready var _title := $Title
+@onready var _names_container := $Names
+
+
+func _set_title(val: String) -> void:
+	title = val
+	if not _title:
+		await ready
+	_title.text = title
+
+
+func _set_names(val: Array[String]) -> void:
+	names = val
+	
+	if not _names_container:
+		await ready
+	
+	for child in _names_container.get_children():
+		child.queue_free()
+	
+	for n in names:
+		var label := Label.new()
+		label.text = n
+		label.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+		_names_container.add_child(label)
