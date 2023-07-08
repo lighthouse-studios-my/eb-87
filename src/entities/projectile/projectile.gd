6ed6e64cbb55beb3d7d8c12893b8eebe7fb72ffa
ZeeHatcher <zee.hatcher010@gmail.com>
2023-07-08 13:28:37 +0800

    feat: add projectile

---
+++ src/entities/projectile/projectile.gd
@@ -0,0 +1,49 @@
+class_name Projectile
+extends Area2D
+
+
+@export var speed := 300
+@export var damage := 1
+
+var direction := Vector2.RIGHT
+
+@onready var context: Node = get_parent()
+
+
+func setup(
+		_damage: int, _direction: Vector2, _speed: int,
+		_position: Vector2, size_scale: float
+) -> void:
+	damage = _damage
+	speed = _speed
+	direction = _direction
+	position = _position
+	
+	$Hitbox.shape.radius = $Hitbox.shape.radius * size_scale
+	$Sprite.scale = $Sprite.scale * size_scale
+
+
+func _physics_process(delta: float) -> void:
+	move(delta)
+
+
+func move(delta: float) -> void:
+	var velocity := speed * direction * delta
+	position += velocity
+
+
+func despawn() -> void:
+	if is_instance_valid(self) and not is_queued_for_deletion():
+		queue_free()
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	despawn()
+
+
+func _on_area_entered(area: Area2D):
+	if is_queued_for_deletion() or area.is_queued_for_deletion():
+		return
+	if area.has_method("hurt"):
+		area.hurt(damage)
+	despawn()
