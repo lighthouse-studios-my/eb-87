7d98b5bab636d2041610d009a05e97b0457647dd
ZeeHatcher <zee.hatcher010@gmail.com>
2023-07-08 21:39:33 +0800

    feat: gameover ui

---
+++ src/ui/gameover/gameover.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+signal restart_pressed
+signal back_pressed
+
+@onready var _buttons := $CenterContainer/VBoxContainer/Buttons.get_children()
+@onready var _cursor := $Cursor
+@onready var _first_button := _buttons[0]
+
+
+func _ready() -> void:
+	for button in _buttons:
+		button.focus_entered.connect(_on_button_focus_entered.bind(button))
+	refocus()
+
+
+func refocus() -> void:
+	_first_button.grab_focus()
+
+
+func _on_button_pressed(button: String) -> void:
+	emit_signal("%s_pressed" % button)
+
+
+func _on_button_focus_entered(control: Control) -> void:
+	_cursor.point_at(control)
+
+
+func _on_buttons_sort_children():
+	_cursor.place_at(_first_button)
