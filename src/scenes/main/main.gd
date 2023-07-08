54868eff256c68c5415ceaff1f1e8c45a281f710
ZeeHatcher <zee.hatcher010@gmail.com>
2023-07-08 16:25:37 +0800

    feat: add upgrade menu to main

--- src/scenes/main/main.gd
+++ src/scenes/main/main.gd
@@ -4,11 +4,12 @@ extends Node2D
 @onready var player := $Player
 @onready var spawner := $Spawner
 @onready var turret := $Turret
+@onready var upgrade_menu := $CanvasLayer/CenterContainer/UpgradeMenu
 
 
 func _ready():
 	pass
-	
+
 
 func _physics_process(delta):
 	if is_instance_valid(player) and not player.is_queued_for_deletion():
@@ -17,3 +18,7 @@ func _physics_process(delta):
 
 func _on_enemy_spawn_timer_timeout():
 	spawner.spawn_pack()
+
+
+func _on_upgrade_menu_upgrade_selected(upgrade):
+	upgrade.apply(turret)
