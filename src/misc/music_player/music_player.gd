9174c7f33d6711fea73b66c683be780235b4b360
Zircon <zircon332@gmail.com>
2023-07-09 09:46:07 +0800

    added music as singleton

--- src/misc/music_player/music_player.gd
+++ src/misc/music_player/music_player.gd
@@ -1,28 +1,10 @@
 extends AudioStreamPlayer
 
 
-var all_music = [
-	load("res://assets/music/Call Me 130 BPM F# Min.mp3"),
-	load("res://assets/music/Fight demo 3 160BPM.mp3"),
-	load("res://assets/music/Fight Me BG 140 BPM C Min.mp3"),
-	load("res://assets/music/Fight_160_BPM_F_Min.mp3"),
-]
+var music = preload("res://assets/music/Fight_160_BPM_F_Min.mp3")
 
-func _unhandled_input(event):
-	if not event is InputEventKey or not event.pressed:
-		return
-	
-	var music: AudioStream
-	if event.keycode == KEY_1:
-		music = all_music[0]
-	elif event.keycode == KEY_2:
-		music = all_music[1]
-	elif event.keycode == KEY_3:
-		music = all_music[2]
-	elif event.keycode == KEY_4:
-		music = all_music[3]
-	else:
-		return
-	
+
+func _ready():
 	stream = music
 	play()
+	process_mode = Node.PROCESS_MODE_ALWAYS
