e560679e7a9f83249a6dc3018cbcde9e2528843d
Zircon <zircon332@gmail.com>
2023-07-09 09:57:22 +0800

    orb fixes

--- src/entities/orb/orb.gd
+++ src/entities/orb/orb.gd
@@ -9,13 +9,16 @@ signal absorbed(exp_points)
 @export var target_location := Vector2(1920/2, 1080/2)
 
 
+func _ready() -> void:
+	attract()
+
+
 func attract() -> void:
 	var tween = create_tween().bind_node(self).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
 	tween.tween_property(self, "position", target_location, 1)
-	tween.tween_callback(self.queue_free)
+	tween.tween_callback(self.absorb)
+
+
+func absorb() -> void:
 	emit_signal("absorbed", exp_points)
-
-
-func _on_body_entered(body):
-	if body.is_in_group("player"):
-		attract()
+	queue_free()
