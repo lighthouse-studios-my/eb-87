08a6c7749f20ad1f158071714388be41fc194429
ZeeHatcher <zee.hatcher010@gmail.com>
2023-07-09 20:23:13 +0800

    feat: credits redesign

--- src/ui/main_menu/screens/credits/credits.gd
+++ src/ui/main_menu/screens/credits/credits.gd
@@ -3,12 +3,10 @@ extends Control
 
 signal back_pressed
 
-@onready var _back_button := $BackButton
-@onready var _cursor := $Cursor
+@onready var _back_button := %BackButton
 
 
 func _ready() -> void:
-	_back_button.focus_entered.connect(_on_button_focus_entered.bind(_back_button))
 	refocus()
 
 
@@ -22,9 +20,10 @@ func refocus() -> void:
 	_back_button.grab_focus()
 
 
-func _on_button_focus_entered(control: Control) -> void:
-	_cursor.point_at(control)
-
-
 func _on_back_button_pressed() -> void:
 	emit_signal("back_pressed")
+	UiSfx.play_ui_select()
+
+
+func _on_back_button_focus_entered():
+	UiSfx.play_ui_select()
